75edc561d17cad5d09af1e005213f1478059785d
Cyril <c.adamo@codeur.online>
2021-02-25 09:39:56 +0100

    travail de Cyril

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,24 +2,22 @@ var Button1 = document.getElementById('switch1')
 var Menu1 = document.getElementById('menu1')
 
 Button1.addEventListener('click' , function(e){
-    e.preventDefault;
     if(Menu1.style.display !='block'){
     Menu1.style.display = 'block'}
     else {
     Menu1.style.display = 'none'
     }
-
+    Menu2.style.display = 'none'
 })
 
 var Button2 = document.getElementById('switch2')
 var Menu2 = document.getElementById('menu2')
 
 Button2.addEventListener('click' , function(e){
-    e.preventDefault;
     if(Menu2.style.display !='block'){
     Menu2.style.display = 'block'}
     else {
     Menu2.style.display = 'none'
     }
-
+    Menu1.style.display = 'none'
 })
