057682f87ed363677da811095e84b8f9588b8ad5
Cyril <c.adamo@codeur.online>
2021-03-02 14:22:51 +0100

    sens commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,7 +8,7 @@ var mybutton = document.getElementById("arrow");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 && window.screen.width > 990) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
